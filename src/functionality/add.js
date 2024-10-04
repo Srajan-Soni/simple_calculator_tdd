e1cae601d99233011e62c505cb52db92844a5eda
Srajan-Soni <60183046+Srajan-Soni@users.noreply.github.com>
2024-10-04 16:09:13 +0530

    test passed for checking single number

--- src/functionality/add.js
+++ src/functionality/add.js
@@ -1,5 +1,9 @@
 
 export function add(numbers) {
     if (!numbers) return 0;
+
+    if(numbers.length>=1){
+        return parseInt(numbers)
+    }
   }
   
--- src/tests/StringCalculator.test.js
+++ src/tests/StringCalculator.test.js
@@ -1,6 +1,15 @@
 import { add } from "../functionality/add";
 
+// Unit test : checking empty string 
 
 test('returns 0 for an empty string', () => {
     expect(add("")).toBe(0);
   });
+
+// Checking number 
+
+
+test('returns number when a single number is passed', () => {
+    expect(add("1")).toBe(1);
+  });
+
