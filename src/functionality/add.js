173e93581a852e53ae1f40649aebf79540adf5f9
Srajan-Soni <60183046+Srajan-Soni@users.noreply.github.com>
2024-10-18 20:57:44 +0530

    refactor the code

--- src/functionality/add.js
+++ src/functionality/add.js
@@ -1,54 +1,57 @@
-
 function escapeRegExp(string) {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
 const getNegativeNums = (str) => {
-    return (str.match(/-\d+/g) || []).map(num => num.trim());
+  return (str.match(/-\d+/g) || []).map((num) => num.trim());
+};
+
+const hasCustomDelimiter = (numbers) => {
+  return numbers.startsWith("//");
 };
 
 export function add(numbers) {
-    if (numbers === "") {
-        return 0;
+  if (numbers === "") {
+    return 0;
+  }
+
+  let delimiter = /[\n,]/;
+
+  if (hasCustomDelimiter(numbers)) {
+    numbers = numbers.replace("\\n", "\n");
+    const parts = numbers.split("\n");
+    if (parts.length > 1) {
+      const customDelimiter = parts[0].slice(2);
+      delimiter = new RegExp(escapeRegExp(customDelimiter), "g");
+      numbers = parts.slice(1).join("\n");
     }
+  } else if (numbers.includes("\\n")) {
+    numbers = numbers.replace("\\n", ",");
+    delimiter = /,/;
+  }
 
-    let delimiter = /[\n,]/;
+  const numsArr = numbers
+    .split(delimiter)
+    .map((num) => num.trim())
+    .filter((num) => num !== "" && !isNaN(num));
 
-    if (numbers.startsWith("//")) {
-        numbers = numbers.replace("\\n", "\n");
-        const parts = numbers.split('\n');
-        if (parts.length > 1) {
-            const customDelimiter = parts[0].slice(2);
-            delimiter = new RegExp(escapeRegExp(customDelimiter), "g");
-            numbers = parts.slice(1).join('\n');
-        }
-    } else if (numbers.includes("\\n")) {
-        numbers = numbers.replace("\\n", ",");
-        delimiter = /,/;
+  let sum = 0;
+  let negativeNumbers = [];
+
+  for (let num of numsArr) {
+    const negatives = getNegativeNums(num);
+    if (negatives.length > 0) {
+      negativeNumbers.push(...negatives);
     }
+  }
 
-    const numsArr = numbers.split(delimiter)
-        .map(num => num.trim())
-        .filter(num => num !== '' && !isNaN(num));
+  if (negativeNumbers.length > 0) {
+    throw new Error(
+      `negative numbers not allowed: ${negativeNumbers.join(",")}`
+    );
+  }
 
-    let sum = 0;
-    let negativeNumbers = [];
+  sum = numsArr.map((nums)=> parseInt(nums,10)).filter((nums)=> !isNaN(nums)).reduce((acc, num) => acc + num, 0);
 
-    for (let num of numsArr) {
-        const negatives = getNegativeNums(num);
-        if (negatives.length > 0) {
-            negativeNumbers.push(...negatives);
-        }
-
-        const x = parseInt(num, 10);
-        if (!isNaN(x)) {
-            sum += x;
-        }
-    }
-
-    if (negativeNumbers.length > 0) {
-        throw new Error(`negative numbers not allowed: ${negativeNumbers.join(",")}`);
-    }
-
-    return sum;
+  return sum;
 }
